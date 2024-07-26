826e232bfea7d5bc96362c62b725d33c50accd17
Hierrohf <hierofigueiredo@gmail.com>
2024-07-26 18:22:21 -0300

    implementing department

---
+++ src/application/program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		Department objt = new  Department(1, "books");
+		
+
+		System.out.println(objt);
+	}
+
+}
