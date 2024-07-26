37a255923cc4ebca3b491cdea161e8bd1a262fe6
Hierrohf <hierofigueiredo@gmail.com>
2024-07-26 18:51:25 -0300

    implementing seller

--- src/application/program.java
+++ src/application/program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class program {
 
@@ -8,8 +11,11 @@ public class program {
 		
 		Department objt = new  Department(1, "books");
 		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, objt);
 
 		System.out.println(objt);
+		
+		System.out.println(seller);
 	}
 
 }
